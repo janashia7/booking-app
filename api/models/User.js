7b97a361a8fbe61108cc6de2c4bb914ad34fb24a
giorgi <gjanashia@unisens.ge>
2022-09-14 18:55:10 +0400

    refactor: create new files for get req and sen res

---
+++ api/models/User.js
@@ -0,0 +1,16 @@
+import { model, Schema } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model('User', UserSchema);
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,8 @@
-import express from 'express';
+import { Router } from 'express';
+import { register } from '../controllers/auth.js';
 
-const router = express.Router();
+const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Auth endpoint');
-});
+router.post('/register', register);
 
 export default router;
