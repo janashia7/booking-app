012bed54033ce1ecede13f60cc0f5ce2ddbc3672
giorgi <gjanashia@unisens.ge>
2022-09-15 23:23:14 +0400

    feat: add hotel rooms functionality

---
+++ api/models/Room.js
@@ -0,0 +1,17 @@
+import { model, Schema } from 'mongoose';
+
+const RoomSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    maxPerson: {
+      type: Number,
+      required: true,
+    },
+    desc: { type: String, required: true },
+    roomNumbers: [{ number: Number, unavailableDates: { type: [Date] } }],
+  },
+  { timestamps: true }
+);
+
+export default model('Room', RoomSchema);
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,23 @@
-import express from 'express';
+import { Router } from 'express';
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from '../controllers/room.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
-const router = express.Router();
+const router = Router();
 
+router.post('/:hotelId', verifyAdmin, createRoom);
+
+router.put('/:id/hotelId', verifyAdmin, updateRoom);
+
+router.delete('/:id', verifyAdmin, deleteRoom);
+
+router.get('/:id', getRoom);
+
+router.get('/', getRooms);
 
 export default router;
